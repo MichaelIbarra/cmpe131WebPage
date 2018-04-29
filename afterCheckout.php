1d50c604914795cc5fec8e67eb66b701f697d84a
michinat <37168439+michinat@users.noreply.github.com>
2018-04-28 21:25:45 -0700

    delete book from database after checkout

---
+++ afterCheckout.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+
+	if(isset($_SESSION['bid']))
+	{
+		$conn = mysqli_connect("localhost", "root", "", "Registration");
+		if($conn -> connect_error)
+    	die("Connection failed:" . $conn -> connect_error);
+$lookBID = $_SESSION['bid'];	
+ $sql = "DELETE FROM books WHERE BID='$lookBID'";
+
+if ($conn->query($sql) === TRUE) {
+	echo ("<SCRIPT LANGUAGE='JavaScript'>
+        	window.alert('Book is bought and has been deleted from the database')
+          window.location.href='searchA.php'
+        	</SCRIPT>");
+} else {
+    echo "Error buying book : " . $conn->error;
+}
+
+$conn->close();
+}
+?>
